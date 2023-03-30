a226a345e619597eac670073988faa6e96c92952
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-30 23:30:59 +0530

    added SOME changes

--- DatabaseSQLFile.sql
+++ DatabaseSQLFile.sql
@@ -21,3 +21,42 @@ mysql> desc HOD;
 
 mysql> select * from hod;
 Empty set (0.01 sec)
+
+
+mysql> drop table HOD;
+Query OK, 0 rows affected (0.07 sec)
+
+mysql> create table HOD(HODId int auto_increment primary key,name varchar(30) not null, username varchar(30) not null, password varchar(30) not null);
+Query OK, 0 rows affected (0.03 sec)
+
+mysql> desc HOD;
++----------+-------------+------+-----+---------+----------------+
+| Field    | Type        | Null | Key | Default | Extra          |
++----------+-------------+------+-----+---------+----------------+
+| HODId    | int         | NO   | PRI | NULL    | auto_increment |
+| name     | varchar(30) | NO   |     | NULL    |                |
+| username | varchar(30) | NO   |     | NULL    |                |
+| password | varchar(30) | NO   |     | NULL    |                |
++----------+-------------+------+-----+---------+----------------+
+4 rows in set (0.01 sec)
+
+mysql> create table complaints( complaintId int not null, empId int,complain
+tType varchar(30), engId int, status varchar(12),dataRaised date, dataResolv
+ed date);
+Query OK, 0 rows affected (0.04 sec)
+
+mysql>  desc complaints;
++---------------+-------------+------+-----+---------+-------+
+| Field         | Type        | Null | Key | Default | Extra |
++---------------+-------------+------+-----+---------+-------+
+| complaintId   | int         | NO   |     | NULL    |       |
+| empId         | int         | YES  |     | NULL    |       |
+| complaintType | varchar(30) | YES  |     | NULL    |       |
+| engId         | int         | YES  |     | NULL    |       |
+| status        | varchar(12) | YES  |     | NULL    |       |
+| dataRaised    | date        | YES  |     | NULL    |       |
+| dataResolved  | date        | YES  |     | NULL    |       |
++---------------+-------------+------+-----+---------+-------+
+7 rows in set (0.00 sec)
+
+mysql>

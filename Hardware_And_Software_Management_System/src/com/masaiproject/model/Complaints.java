09951a8a61cbeb30804932e3c30d1ab32ced22e8
Vishal6300 <vkdeoghar1998@gmail.com>
2023-03-31 23:01:00 +0530

    added Complaints class

---
+++ Hardware_And_Software_Management_System/src/com/masaiproject/model/Complaints.java
@@ -0,0 +1,15 @@
+package com.masaiproject.model;
+
+import java.sql.Date;
+
+public class Complaints {
+	private int complaintId;
+	private int empId;
+	private String complaintType;
+	private int engId;
+	private String status;
+	private Date dateRaised;
+	private Date dateResolved;
+	
+	public Complaints() {}
+}

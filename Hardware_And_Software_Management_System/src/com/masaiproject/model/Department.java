3b5ddab8c85dd025bbfe58244d87c4f8aaf517d8
Vishal6300 <vkdeoghar1998@gmail.com>
2023-04-01 09:50:50 +0530

    added some  classes

--- Hardware_And_Software_Management_System/src/com/masaiproject/model/Department.java
+++ Hardware_And_Software_Management_System/src/com/masaiproject/model/Department.java
@@ -5,6 +5,44 @@ public class Department {
 	private String dName;
 	private String location;
 	
-	public Department() {}
+	public Department() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Department [deptid=" + deptid + ", dName=" + dName + ", location=" + location + "]";
+	}
+
+	public Department(int deptid, String dName, String location) {
+		super();
+		this.deptid = deptid;
+		this.dName = dName;
+		this.location = location;
+	}
+
+	public int getDeptid() {
+		return deptid;
+	}
+
+	public void setDeptid(int deptid) {
+		this.deptid = deptid;
+	}
+
+	public String getdName() {
+		return dName;
+	}
+
+	public void setdName(String dName) {
+		this.dName = dName;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
 }
